aa6953e9dc9bd5b1a848e3b1ed32f85fc00aa2ba
sahaza <sakaizanomena@gmail.com>
2026-02-06 13:57:14 +0100

    FE - fix bug de couleur sur le composant item

--- src/components/ComponentItem/index.tsx
+++ src/components/ComponentItem/index.tsx
@@ -7,7 +7,7 @@ const ComponentItem = ({
 }: IcomponentLists) => {
 
   return (
-    <div className="w-full h-16 bg-gray-200 rounded-lg mb-4 flex items-center justify-center" id={key}>
+    <div className="w-full h-12 bg-zinc-300 border border-zinc-400 text-zinc-800 dark:text-zinc-50 dark:bg-gray-900 dark:border-gray-950 font-semibold rounded-lg mb-4 flex items-center justify-center hover:scale-[103%] transition-transform ease-in-out duration-300 cursor-pointer" id={key}>
       {icon} {title} 
     </div>
   )
