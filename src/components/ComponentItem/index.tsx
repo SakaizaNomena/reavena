ea1976a619bf4c41b7a00cdda9864b68c28890f4
sahaza <sakaizanomena@gmail.com>
2026-02-06 15:44:02 +0100

    FE - Création des dossiers de base

--- src/components/ComponentItem/index.tsx
+++ src/components/ComponentItem/index.tsx
@@ -7,10 +7,9 @@ const ComponentItem = ({
   activeKey,
   onClick
 }: IcomponentLists) => {
-  console.log('activeKey', activeKey);
   return (
     <div 
-      className={`w-full h-12  text-zinc-800 dark:text-zinc-50  font-semibold rounded-l-3xl mb-4 flex items-center justify-center hover:scale-[103%] transition-transform ease-in-out duration-300 cursor-pointer ${keyItem === activeKey ? `dark:bg-gray-900 dark:border-gray-950/10 bg-gray-100` : ''}`} 
+      className={`w-full h-12  text-zinc-800 dark:text-zinc-50  font-semibold rounded-l-3xl mb-4 flex items-center justify-start pl-4 hover:scale-[103%] transition-transform ease-in-out duration-300 cursor-pointer hover:dark:bg-gray-900 hover:dark:border-gray-950/10 hover:bg-gray-100 ${keyItem === activeKey ? `dark:bg-gray-900 dark:border-gray-950/10 bg-gray-100` : ''}`} 
       id={keyItem}
       onClick={() => onClick && onClick(keyItem)}
     >
